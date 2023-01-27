174ae4e43b04d03e7f79ff6a5babd4151dc340f2
Jonthesallz <gitboi56@outlook.com>
2023-01-27 11:55:28 +0000

    about section done

--- src/constants/data.tsx
+++ src/constants/data.tsx
@@ -1,6 +1,16 @@
+import html from "../images/logos/html.png";
+import css from "../images/logos/css.png";
+import javascript from "../images/logos/javascript.png";
+import reactImage from "../images/logos/react.png";
+import nextjs from "../images/logos/nextjs.png";
+import node from "../images/logos/node.png";
+import typscript from "../images/logos/typescript.png";
+import tailwind from "../images/logos/tailwind.png";
 
 
-type Portfoliotype = 
+
+
+type Portfoliotype =
     {
         id: number,
         title: string,
@@ -28,23 +38,83 @@ export const Portfolio: Portfoliotype[] = [
 
 
 type paraType = {
-    id:number,
-    para:string,
+    id: number,
+    para: string,
 }
 
-export const aboutPara:paraType[] = [
+export const aboutPara: paraType[] = [
     {
         id: 1,
         para: "Hi, I'm Jonathan. A self taught Full Stack Developer. I have a passion for creating responsive websites and webapps. My programming language is Javascript. A powerfull tool at my disposal which allows my creativity to come alive. Have a look in the Projects section where I get to show off my skills.",
-        
+
     },
     {
         id: 2,
         para: "I am currently exploring react libraries and building new things with them. I'll be continously updating my projects section as I create cool projects. When I'm not coding I am going to the gym, watching anime, movies and consuming as much football as possible!",
     },
     {
-        id:3,
+        id: 3,
         para: " I am open to Job opportunities where I can grow, contribute and continue to learn."
     }
 
+]
+
+
+
+type skillsType = {
+    id: number,
+    src: string,
+    title: string,
+    style: string
+}
+
+export const skillsInfo: skillsType[] = [
+    {
+        id: 1,
+        src: html,
+        title: "HTML",
+        style: "dark:shadow-orange-500",
+    },
+    {
+        id: 2,
+        src: css,
+        title: "CSS",
+        style: "dark:shadow-blue-500",
+    },
+    {
+        id: 3,
+        src: javascript,
+        title: "JavaScript",
+        style: "dark:shadow-yellow-500",
+    },
+    {
+        id: 4,
+        src: reactImage,
+        title: "React",
+        style: "dark:shadow-blue-600",
+    },
+    {
+        id: 5,
+        src: tailwind,
+        title: "Tailwind",
+        style: "dark:shadow-sky-400",
+    },
+    {
+        id: 6,
+        src: nextjs,
+        title: "Next JS",
+        style: "dark:shadow-white",
+    },
+    {
+        id: 7,
+        src: node,
+        title: "Node JS",
+        style: "dark:shadow-[#66cc33]",
+    },
+    {
+        id: 8,
+        src: typscript,
+        title: "TypeScript",
+        style: "dark:shadow-[#3178C6]",
+    },
 ]
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,22 +1,40 @@
 import { aboutPara } from "../constants/data"
 import ParaText from "../components/ParaText"
+import { skillsInfo } from "../constants/data"
 
 const About = () => {
   return (
-    <section id='about' className="py-5  dark:bg-black ">
-      <div className=' max-w-5xl mx-auto p-4 py-16'>
+    <section id='about' className="py-5  dark:bg-neutral-800 ">
+      <div className=' max-w-5xl mx-auto p-4 py-16 md:pl-20'>
         <div className="pb-10">
-        <h2 className=" md:text-4xl text-2xl font-bold text-center text-sky-700 border-b-4 border-sky-700 inline">About</h2>
+          <h2 className=" md:text-4xl text-2xl font-bold text-center text-sky-700 border-b-4 border-sky-700 inline">About</h2>
         </div>
-      
+
         {aboutPara.map((aboutpara) => (
-          <ParaText  key={aboutpara.id}>{aboutpara.para}</ParaText>
+          <ParaText key={aboutpara.id}>{aboutpara.para}</ParaText>
         ))}
         <div className="py-10">
-        <h3 className=" mt-10 md:text-3xl text-2xl font-bold text-center text-sky-700 border-sky-700 border-b-4 pb-2 inline">Skills</h3>
+          <h3 className=" mt-10 md:text-3xl text-2xl font-bold text-center text-sky-700 border-sky-700 border-b-4 pb-2 inline">Skills</h3>
         </div>
-       
+
         <p className="py-4 text-stone-400 text-base ">Here are some of the technolgies I worked with</p>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 py-8 px-12 sm:px-0">
+          {skillsInfo.map(({id,src,title,style})=> (
+              <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg  shadow-black/50 ${style}`} key={id}>
+              <img src={src} alt="" className="w-20 mx-auto" />
+              <p className="text-center mt-4 font-semibold text-stone-400">{title}</p>
+            </div>
+          
+
+          ))}
+          </div>
+        
+
+
+
+
+
       </div>
 
     </section>
