89bebae7c48bc39c71f0e2e30f6e780a74d19348
Jonthesallz <gitboi56@outlook.com>
2023-01-27 10:24:46 +0000

    addes logos to images

--- src/constants/data.tsx
+++ src/constants/data.tsx
@@ -2,6 +2,7 @@
 
 type Portfoliotype = 
     {
+        id: number,
         title: string,
         subheading: string,
         image: string
@@ -10,11 +11,13 @@ type Portfoliotype =
 
 export const Portfolio: Portfoliotype[] = [
     {
+        id: 1,
         title: "yelpcamp",
         subheading: "A Full-stack web applicaton made using Node.js for the back-end and MongoDB Atlas as a cloud datbase. Yelpcamp allows users to create, review and delete campgorunds. You have to start by creating an account to utilise.",
         image: "hello"
     },
     {
+        id: 2,
         title: "Todo App",
         subheading: "It can be difficult to remember everything we need to do. This is where this Todo App comes in handy. A functioning crud application that allows users to stores tasks on MongoDD that are instanly dispalyed on the page with ejs.",
         image: "dashf"
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -3,16 +3,16 @@ import ParaText from "../components/ParaText"
 
 const About = () => {
   return (
-    <div id='about' className="py-5  dark:bg-black ">
+    <section id='about' className="py-5  dark:bg-black ">
       <div className=' max-w-5xl mx-auto p-4 py-16'>
         <h2 className=" md:text-4xl text-2xl font-bold text-center text-sky-700">About</h2>
         {aboutPara.map((aboutpara) => (
           <ParaText  key={aboutpara.id}>{aboutpara.para}</ParaText>
-          
         ))}
+        <h3 className=" my-10 md:text-3xl text-1.5xl font-bold text-center text-sky-700">Skills</h3>
       </div>
 
-    </div>
+    </section>
   )
 }
 
