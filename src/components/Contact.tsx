77dd54e5541a2e28ea3e2ef7668e596042df414c
Jonthesallz <gitboi56@outlook.com>
2023-01-27 23:26:48 +0000

    changed the contact section for dark mode

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,12 +8,25 @@ export const Contact = () => {
                     <h2 className=" md:text-4xl text-2xl font-bold text-center text-sky-700 border-b-4 border-sky-700 inline">Contact</h2>
                 </div>
                 <form action='' method='POST' encType="multipart/form-data" >
-                    <div>
-                        <div>
-                            <label>Name</label>
-                            <input type="text" name="name" />
-                        </div>
+
+                    <div className='flex flex-col py-2'>
+                        <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Name</label>
+                        <input type="text" name="name"
+                            className='border-2 rounded-lg p-3 border-gray-300  flex dark:bg-stone-800 dark:text-white' />
                     </div>
+                    <div className='flex flex-col py-2 '>
+                        <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Email</label>
+                        <input className='border-2 rounded-lg p-3 dark:bg-stone-800 dark:text-white' type="email" name="email" />
+                    </div>
+                    <div className='flex flex-col py-2'>
+                        <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Subject</label>
+                        <input className='border-2 rounded-lg p-3 border-gray-300  dark:bg-stone-800 dark:text-white' type="text" name="subject" />
+                    </div>
+                    <div className='flex flex-col py-2'>
+                        <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Message</label>
+                        <textarea className='accent-neutral-300 border-2 rounded-lg p-3 border-gray-300 resize-none dark:bg-stone-800 dark:text-white' rows={10} name="message"></textarea>
+                    </div>
+                    <button className='bg-sky-800' />
                 </form>
             </div>
 
--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -9,7 +9,7 @@ type PropjectItemProps = {
 
 const ProjectItem = ({ title, demo, github, image }: PropjectItemProps) => {
   return (
-    <div className=' relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-sky-600'>
+    <div className=' relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 dark:shadow-gray-700    rounded-xl group hover:bg-gradient-to-r from-gray-200 to-sky-600'>
       <img src={image} alt={title} className='aspect-[4/3] object-cover rounded-xl group-hover:opacity-10' />
       <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%]  translate-y-[-50%]'>
         <h3 className='font-bold text-2xl text-white tracking-wider text-center'>
