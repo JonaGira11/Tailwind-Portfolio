9dedeef50ee4d1924dc8771006ed6738cd343b74
Jonthesallz <gitboi56@outlook.com>
2023-01-30 15:15:23 +0000

    fixed form submit and navbar on small screens

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,7 +7,7 @@ export const Contact = () => {
                 <div className="pb-10">
                     <h2 className=" md:text-4xl text-2xl font-bold text-center text-sky-700 border-b-4 border-sky-700 inline">Contact</h2>
                 </div>
-                <form action='' method='POST' encType="multipart/form-data" >
+                <form action='https://getform.io/f/58db4c0c-b262-405f-ba7b-0adf98c43ef0' method='POST' encType="multipart/form-data" >
 
                     <div className='flex flex-col py-2'>
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Name</label>
@@ -26,7 +26,7 @@ export const Contact = () => {
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Message</label>
                         <textarea className='accent-neutral-300 border-2 rounded-lg p-3 border-gray-300 resize-none dark:bg-stone-800 dark:text-white' rows={10} name="message"></textarea>
                     </div>
-                    <button className='bg-sky-800' >Send Message</button>
+                    <button className='bg-sky-800 text-gray-100 mt-4 w-full rounded-lg p-4' type='submit'>Send Message</button>
                 </form>
             </div>
 
--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -25,19 +25,21 @@ const Sidenav = () => {
             <AiOutlineMenu size={25} className=" cursor-pointer absolute top-4 right-4 z-[99] md:hidden dark:text-white" onClick={handleNav} />{
                 nav ? (
                     <div className="fixed w-full h-screen bg-white flex flex-col justify-center items-center z-20 md:hidden dark:bg-zinc-700">
-                        <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in dark:bg-zinc-600  dark:text-white dark:shadow-zinc-900 ">
+                        <a 
+                         onClick={handleNav}  
+                        href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in dark:bg-zinc-600  dark:text-white dark:shadow-zinc-900 ">
                             <AiOutlineHome size={20} />
                             <span className="pl-4">Home</span>
                         </a>
-                        <a href="#about" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in dark:bg-zinc-600  dark:text-white dark:shadow-zinc-900">
+                        <a onClick={handleNav} href="#about" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in dark:bg-zinc-600  dark:text-white dark:shadow-zinc-900">
                             <AiOutlineUser size={20} />
                             <span className="pl-4">About</span>
                         </a>
-                        <a href="#projects" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in dark:bg-zinc-600  dark:text-white dark:shadow-zinc-900">
+                        <a  onClick={handleNav}  href="#projects" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in dark:bg-zinc-600  dark:text-white dark:shadow-zinc-900">
                             <AiOutlineProject size={20} />
                             <span className="pl-4">Projects</span>
                         </a>
-                        <a href="#contact" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in dark:bg-zinc-600  dark:text-white dark:shadow-zinc-900">
+                        <a onClick={handleNav}  href="#contact" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in dark:bg-zinc-600  dark:text-white dark:shadow-zinc-900">
                             <AiOutlineMail size={20} />
                             <span className="pl-4">Contact</span>
                         </a>
