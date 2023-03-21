73c336bd199458f493349f6f775d0078bbc18af5
jj-sallz89 <gitboi56@outlook.com>
2023-03-21 14:07:34 +0000

    changed contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -19,31 +19,35 @@ export const Contact = () => {
                 </div>
 
                 <SubheadingText text="Get in contact with me " />
-
-                <motion.form action='https://getform.io/f/58db4c0c-b262-405f-ba7b-0adf98c43ef0' method='POST' encType="multipart/form-data"
-                    viewport={{ once: true }}
-                    initial="hidden"
-                    whileInView="show"
-                    variants={formVariants}>
-                    <div className='flex flex-col pb-2 pt-8'>
+<motion.div className="bg-gray-500/30 p-4 border max-w-[500px] mx-auto rounded-xl border-none mt-4"
+  viewport={{ once: true }}
+  initial="hidden"
+  whileInView="show"
+  variants={formVariants}>
+                <form action='https://getform.io/f/58db4c0c-b262-405f-ba7b-0adf98c43ef0' method='POST' encType="multipart/form-data">
+                       
+                    <div className='flex flex-col  '>
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Name</label>
                         <input type="text" name="name" required
                             className='border-2 rounded-lg p-3 border-gray-300  flex dark:bg-neutral-800 dark:text-white' />
                     </div>
-                    <div className='flex flex-col py-2 '>
+                    <div className='flex flex-col  '>
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Email</label>
-                        <input className='border-2 rounded-lg p-3 border-gray-300  dark:bg-neutral-800 dark:text-white' type="email" name="email" required />
+                        <input className='max-w-[600px] border-2 rounded-lg p-3 border-gray-300  dark:bg-neutral-800 dark:text-white' type="email" name="email" required />
                     </div>
-                    <div className='flex flex-col py-2'>
+                    <div className='flex flex-col'>
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Subject</label>
                         <input className='border-2 rounded-lg p-3 border-gray-300  dark:bg-neutral-800 dark:text-white' type="text" name="subject"  required/>
                     </div>
-                    <div className='flex flex-col py-2'>
+                    <div className='flex flex-col'>
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Message</label>
                         <textarea className='accent-neutral-300 border-2 rounded-lg p-3 border-gray-300 resize-none dark:bg-neutral-800 dark:text-white' rows={5} name="message" required></textarea>
                     </div>
-                    <button className='bg-sky-800 text-gray-100 mt-4 w-full rounded-lg p-4 hover:bg-sky-900' type='submit'>Send Message</button>
-                </motion.form>
+                    <div className="grid place-content-center">
+                    <button className='bg-sky-800 text-gray-100 mt-4  w-60 rounded-lg p-4 hover:bg-sky-900 ' type='submit'>Send Message</button>
+                    </div>
+                </form>
+                </motion.div>
             </motion.div>
         </section>
     )
