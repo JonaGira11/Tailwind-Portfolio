57927e4344bd54ac6a6ce0d810684f1f4e8202d4
jj-sallz89 <gitboi56@outlook.com>
2023-03-24 21:48:30 +0000

    added toast

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,10 +5,11 @@ import {z, ZodType} from "zod"
 import {useForm} from "react-hook-form"
 import {zodResolver} from "@hookform/resolvers/zod"
 import emailjs from '@emailjs/browser';
-import { ReactEventHandler, useRef } from "react"
+import { useRef } from "react"
+import {  toast } from 'react-toastify';
+
 
 export const Contact = () => {
-
 
 const form = useRef<HTMLFormElement>(null)
 type FormData = {
@@ -28,11 +29,10 @@ const {handleSubmit, register, formState: {errors}} = useForm<FormData>({ resolv
 const submitData = (data:FormData) => {
 
 emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current as HTMLFormElement, import.meta.env.VITE_PUBLIC_KEY )
-alert("message sent")
+toast("message sent 👍",)
 form.current?.reset()
 }
-
-    return (
+   return (
         <section id='contact' className="pt-3 dark:bg-neutral-800 " aria-label='contact section'>
             <motion.div className=' max-w-5xl mx-auto p-4  pt-8 md:pt-10 md:pl-20 '
                 variants={outterContainer}
@@ -48,11 +48,12 @@ form.current?.reset()
                 </div>
 
                 <SubheadingText text="Get in contact with me " />
-<motion.div className="bg-gray-500/30 p-4 border max-w-[500px] mx-auto rounded-xl border-none mt-4 shadow-md shadow-gray-400 dark:shadow-black/20"
-  viewport={{ once: true }}
-  initial="hidden"
-  whileInView="show"
-  variants={formVariants}>
+
+            <motion.div className="bg-gray-500/30 p-4 border max-w-[500px] mx-auto rounded-xl border-none mt-4 shadow-md shadow-gray-400 dark:shadow-black/20"
+            viewport={{ once: true }}
+            initial="hidden"
+            whileInView="show"
+            variants={formVariants}>
                 <form   ref={form} onSubmit={handleSubmit(submitData)}>
                        
                     <div className='flex flex-col  '>
@@ -73,14 +74,14 @@ form.current?.reset()
                     </div>
                     <div className='flex flex-col'>
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Message</label>
-                        <textarea className='accent-neutral-300 border-2 rounded-lg p-3 border-gray-300 resize-none dark:bg-neutral-800 dark:text-white' rows={5} {...register("message")} required></textarea>
+                        <textarea className='accent-neutral-300 border-2 rounded-lg p-3 border-gray-300 resize-none dark:bg-neutral-800 dark:text-white' rows={3} {...register("message")} required></textarea>
                         {errors.message && <span className="text-red-600">{errors.message.message}</span>}
                     </div>
                     <div className="grid place-content-center">
                     <button className='bg-sky-800 text-gray-100 mt-4  w-60 rounded-lg p-4 hover:bg-sky-900 ' type='submit'>Send Message</button>
                     </div>
                 </form>
-                </motion.div>
+            </motion.div>
             </motion.div>
         </section>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <ToastContainer  
+    toastClassName={()=> "bg-black"}
+      position="top-center"
+      autoClose={2500}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover={false} />
   </React.StrictMode>,
 )
