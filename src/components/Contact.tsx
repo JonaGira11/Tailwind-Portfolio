4bc5004db39177f50f8a2632a21ed209e5781f5f
jj-sallz89 <gitboi56@outlook.com>
2023-03-24 16:47:46 +0000

    adding form validation with zod and react hook form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,29 @@
 import { motion } from "framer-motion"
 import SubheadingText from "./SubheadingText"
 import { outterContainer, headingVariants, formVariants } from "../utils/motion"
+import {z, ZodType} from "zod"
+import {useForm} from "react-hook-form"
+import {zodResolver} from "@hookform/resolvers/zod"
 
 export const Contact = () => {
+type FormData = {
+    name: string;
+    email: string;
+    subject:string;
+    message: string;
+};
+const schema:ZodType<FormData> = z.object({
+    name: z.string().min(2).max(30),
+    email: z.string().email(),
+    subject: z.string().min(2).max(30),
+    message: z.string().min(3).max(150),
+});
+const {handleSubmit, register, formState: {errors}} = useForm<FormData>({ resolver: zodResolver(schema)})
+
+const submitData = (data:FormData) => {
+console.log(data)
+}
+
     return (
         <section id='contact' className="pt-3 dark:bg-neutral-800 " aria-label='contact section'>
             <motion.div className=' max-w-5xl mx-auto p-4  pt-8 md:pt-10 md:pl-20 '
@@ -24,24 +45,28 @@ export const Contact = () => {
   initial="hidden"
   whileInView="show"
   variants={formVariants}>
-                <form action='https://getform.io/f/58db4c0c-b262-405f-ba7b-0adf98c43ef0' method='POST' encType="multipart/form-data">
+                <form action='https://getform.io/f/58db4c0c-b262-405f-ba7b-0adf98c43ef0' method='POST' encType="multipart/form-data" onSubmit={handleSubmit(submitData)}>
                        
                     <div className='flex flex-col  '>
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Name</label>
-                        <input type="text" name="name" required
+                        <input type="text"  required {...register("name")}
                             className='border-2 rounded-lg p-3 border-gray-300  flex dark:bg-neutral-800 dark:text-white' />
+                            {errors.name && <span className="text-red-600">{errors.name.message}</span>}
                     </div>
                     <div className='flex flex-col  '>
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Email</label>
-                        <input className='max-w-[600px] border-2 rounded-lg p-3 border-gray-300  dark:bg-neutral-800 dark:text-white' type="email" name="email" required />
+                        <input className='max-w-[600px] border-2 rounded-lg p-3 border-gray-300  dark:bg-neutral-800 dark:text-white' type="email"  required {...register("email")} />
+                        {errors.email && <span className="text-red-600">{errors.email.message}</span>}
                     </div>
                     <div className='flex flex-col'>
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Subject</label>
-                        <input className='border-2 rounded-lg p-3 border-gray-300  dark:bg-neutral-800 dark:text-white' type="text" name="subject"  required/>
+                        <input className='border-2 rounded-lg p-3 border-gray-300  dark:bg-neutral-800 dark:text-white' type="text" {...register("subject")}  required/>
+                        {errors.subject && <span className="text-red-600">{errors.subject.message}</span>}
                     </div>
                     <div className='flex flex-col'>
                         <label className='uppercase text-sm py-2 text-stone-400 font-semibold'>Message</label>
-                        <textarea className='accent-neutral-300 border-2 rounded-lg p-3 border-gray-300 resize-none dark:bg-neutral-800 dark:text-white' rows={5} name="message" required></textarea>
+                        <textarea className='accent-neutral-300 border-2 rounded-lg p-3 border-gray-300 resize-none dark:bg-neutral-800 dark:text-white' rows={5} {...register("message")} required></textarea>
+                        {errors.message && <span className="text-red-600">{errors.message.message}</span>}
                     </div>
                     <div className="grid place-content-center">
                     <button className='bg-sky-800 text-gray-100 mt-4  w-60 rounded-lg p-4 hover:bg-sky-900 ' type='submit'>Send Message</button>
